21b6d7a55908a8ce4fe66f4e0b91a7ff1a8f44ab
Ahmad-Imam <ahmadimam@iut-dhaka.edu>
2022-10-19 15:59:59 +0300

    google api called and showed

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var AuthController = require('../src/controllers/auth/auth.controller')
 
+var RestaurantController = require('../src/controllers/restaurant/restaurant.controller')
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
@@ -12,6 +14,7 @@ router.get('/', function (req, res, next) {
 router.post('/signup', AuthController.auth);
 router.post('/login', AuthController.login);
 router.post(`/verify/:token`, AuthController.verify);
+router.post(`/getRestaurantList`, RestaurantController.getRestaurantList);
 
 
 
