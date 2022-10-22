252edf5f64e5759e4ee81b7bc4b1eaaeb645866b
Zeba Ramzan <zebaramzan@hotmail.com>
2022-10-22 23:29:26 +0300

    get nearby restaurants list

--- src/controllers/group/group.controller.js
+++ src/controllers/group/group.controller.js
@@ -1,203 +1,134 @@
 const Models = require('../../../models');
 const moment = require('moment');
-const {
-    Op
-} = require('sequelize')
-const {
-    Group,
-    GroupMember,
-    User,
-    Restaurant
-} = Models;
+const sequelize = require('sequelize');
+const Sequelize = require('../../../config/config');
+const { Op, QueryTypes } = sequelize;
+const { Group, GroupMember, User, Restaurant } = Models;
 
 class GroupController {
-
-    createGroup(request, response, next) {
-
-        try {
-            Group.create({
-                time: new moment(request.body.time),
-                officeId: request.body.officeId,
-                restaurantId: request.body.restaurantId
-            }).then(async (group) => {
-                if (group) {
-                    await User.findOne({
-                        attributes: ['id'],
-                        where: {
-                            authToken: request.body.authToken,
-                        },
-                    }).then(async (user) => {
-                        if (user) {
-
-                            GroupMember.create({
-                                userId: user.id,
-                                groupId: group.id,
-                            })
-                            console.log(user.id);
-                            response.send({
-                                status: 200,
-                                message: "Success",
-                            })
-                        } else {
-                            console.log('not creating group members');
-                        }
-                    })
-                } else {
-                    console.log('not');
-                }
-            });
-
-        } catch (error) {
-            next(error);
-        }
-        return undefined;
-    }
-
-
-
-
-    createRandomGroup(request, response, next) {
-
-        try {
-
-            let restaurantList = [];
-
-
-            Restaurant.findAll({
-                attributes: ['id'],
-                where: {
-                    nearByOffice: {
-                        [Op.contains]: [request.body.nearByOffice]
-                    },
-                },
-            }).then(async (restaurant) => {
-
-                response.send({
-                    status: 200,
-                    message: "Success",
-                    data: restaurant,
-                })
-
-
-
-            }).catch(error => {
-                console.log(error);
-            })
-
-
-            ///////////////////////////////////////
-            // Group.create({
-            //     time: new moment(request.body.time),
-            //     officeId: request.body.officeId,
-            // }).then(async (group) => {
-            //     if (group) {
-            //         await User.findOne({
-            //             attributes: ['id'],
-            //             where: {
-            //                 authToken: request.body.authToken,
-            //             },
-            //         }).then(async (user) => {
-            //             if (user) {
-
-            //                 GroupMember.create({
-            //                     userId: user.id,
-            //                     groupId: group.id,
-            //                 })
-            //                 console.log(user.id);
-            //                 response.send({
-            //                     status: 200,
-            //                     message: "Success",
-            //                 })
-            //             } else {
-            //                 console.log('not creating group members');
-            //             }
-            //         })
-            //     } else {
-            //         console.log('not');
-            //     }
-            // });
-
-        } catch (error) {
-            next(error);
-        }
-        return undefined;
-    }
-
-
-
-
-
-
-
-    async getGroupList(request, response, next) {
-        // currentDate = new moment();
-        await User.findOne({
-            attributes: ['id', 'officeId', 'authToken'],
+  createGroup(request, response, next) {
+    try {
+      Group.create({
+        time: new moment(request.body.time),
+        officeId: request.body.officeId,
+        restaurantId: request.body.restaurantId,
+      }).then(async (group) => {
+        if (group) {
+          await User.findOne({
+            attributes: ['id'],
             where: {
-                authToken: request.body.authToken,
+              authToken: request.body.authToken,
             },
-        }).then(async (user) => {
+          }).then(async (user) => {
             if (user) {
-
-                Group.findAll({
-                    attributes: ['id', 'officeId', 'restaurantId'],
-                    where: {
-                        officeId: user.officeId,
-                        time: {
-                            [Op.gte]: new Date()
-                        }
-                    },
-                    include: [{
-                        model: GroupMember,
-                        as: 'groupMember',
-                        attributes: ['userId'],
-                        include: [{
-                            model: User,
-                            as: 'user',
-                            attributes: ['name']
-                        }]
-                    }, {
-                        model: Restaurant,
-                        as: 'restaurant',
-                        attributes: ['name']
-                    }]
-                }).then((result) => {
-
-
-                    response.send({
-                        status: 200,
-                        message: "Success",
-                        data: result,
-                    })
-
-                }).catch(error => {
-                    console.log(error);
-                })
-
-
+              GroupMember.create({
+                userId: user.id,
+                groupId: group.id,
+              });
+              console.log(user.id);
+              response.send({
+                status: 200,
+                message: 'Success',
+              });
             } else {
-                response.send({
-                    status: 400,
-                    message: "Could not get user",
-                })
+              console.log('not creating group members');
             }
-        }).catch(error => {
-            console.log(error);
-        })
-
+          });
+        } else {
+          console.log('not');
+        }
+      });
+    } catch (error) {
+      next(error);
     }
+    return undefined;
+  }
 
+  async createRandomGroup(request, response, next) {
+    try {
+      const nearByRestaurants = await Sequelize.query(
+        `SELECT id FROM restaurants WHERE JSON_CONTAINS(nearby_office, \'[${request.body.nearByOffice}]\')`,
+        {
+          type: QueryTypes.SELECT,
+        }
+      ).then((resp) => {
+        if (resp) {
+          return resp.map((rest) => rest.id);
+        } else return [];
+      });
 
-
-    joinGroup() {
-
+      response.send({
+        status: 200,
+        message: 'Success',
+        data: nearByRestaurants,
+      });
+    } catch (error) {
+      next(error);
     }
-    joinRandomGroup() {
+    return undefined;
+  }
 
-    }
+  async getGroupList(request, response, next) {
+    await User.findOne({
+      attributes: ['id', 'officeId', 'authToken'],
+      where: {
+        authToken: request.body.authToken,
+      },
+    })
+      .then(async (user) => {
+        if (user) {
+          Group.findAll({
+            attributes: ['id', 'officeId', 'restaurantId'],
+            where: {
+              officeId: user.officeId,
+              time: {
+                [Op.gte]: new Date(),
+              },
+            },
+            include: [
+              {
+                model: GroupMember,
+                as: 'groupMember',
+                attributes: ['userId'],
+                include: [
+                  {
+                    model: User,
+                    as: 'user',
+                    attributes: ['name'],
+                  },
+                ],
+              },
+              {
+                model: Restaurant,
+                as: 'restaurant',
+                attributes: ['name'],
+              },
+            ],
+          })
+            .then((result) => {
+              response.send({
+                status: 200,
+                message: 'Success',
+                data: result,
+              });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        } else {
+          response.send({
+            status: 400,
+            message: 'Could not get user',
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-
-
+  joinGroup() {}
+  joinRandomGroup() {}
 }
 
 module.exports = new GroupController();
