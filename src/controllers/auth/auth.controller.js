3794fe5afcb02ae8ce7bc1b5c0d9e4d87bc3bae9
Muhammad Arslan Shahab <muhammadarslan.shahab@tuni.fi>
2022-10-22 12:36:13 +0300

    changes committed

--- src/controllers/auth/auth.controller.js
+++ src/controllers/auth/auth.controller.js
@@ -11,9 +11,11 @@ class AuthController {
   async auth(request, response, next) {
     try {
       const { email, password } = request.body;
+
       await User.findOne({
+        // attributes: ['email'],
         where: {
-          email,
+          email: request.body.email,
         },
       }).then(async (user) => {
         ///check if email already exists
@@ -58,40 +60,84 @@ class AuthController {
       var email = request.body.email;
       var password = request.body.password;
 
-      db.query(
-        `select password, verified from users where email = '${email}'`,
-        function (err, result) {
-          if (result && result.length > 0) {
-            if (!result[0].verified) {
-              return response.send({
-                status: 400,
-                message: 'Email is not verified',
-              });
-            }
+      /////////////////////////////////////
+      await User.findOne({
+        attributes: ['password','verified','authToken'],
+        where: {
+          email: request.body.email,
+        
+        },
+      }).then(async (result) => {
 
-            let dbPassword = result[0].password;
-            dbPassword = dbPassword.replace(/^\$2y(.+)$/i, '$2a$1');
-            if (bcrypt.compareSync(password, dbPassword)) {
-              // log user in
-              return response.send({
-                status: 200,
-                message: 'login done',
-              });
-            } else {
-              // password does not match
-              return response.send({
-                status: 400,
-                message: 'email or password does not match',
-              });
-            }
-          } else {
+        if (result) {
+          if (!result.verified) {
             return response.send({
               status: 400,
-              message: 'email or passord does not match',
+              message: 'Email is not verified',
             });
           }
+
+          let dbPassword = result.password;
+          dbPassword = dbPassword.replace(/^\$2y(.+)$/i, '$2a$1');
+          if (bcrypt.compareSync(password, dbPassword)) {
+            // log user in
+            return response.send({
+              status: 200,
+                authToken: result.authToken,
+              message: 'login done',
+            });
+          } else {
+            // password does not match
+            return response.send({
+              status: 400,
+              message: 'password does not match',
+            });
+          }
+        } else {
+          return response.send({
+            status: 400,
+            message: 'email or password does not match',
+          });
         }
-      );
+      })
+
+      /////////////////////////////////////////////////////////
+
+
+      // db.query(
+      //   `select password, verified from users where email = '${email}'`,
+      //   function (err, result) {
+      //     if (result && result.length > 0) {
+      //       if (!result[0].verified) {
+      //         return response.send({
+      //           status: 400,
+      //           message: 'Email is not verified',
+      //         });
+      //       }
+
+      //       let dbPassword = result[0].password;
+      //       dbPassword = dbPassword.replace(/^\$2y(.+)$/i, '$2a$1');
+      //       if (bcrypt.compareSync(password, dbPassword)) {
+      //         // log user in
+      //         return response.send({
+      //           status: 200,
+      //           message: 'login done',
+      //         });
+      //       } else {
+      //         // password does not match
+      //         return response.send({
+      //           status: 400,
+      //           message: 'email or password does not match',
+      //         });
+      //       }
+      //     } else {
+      //       return response.send({
+      //         status: 400,
+      //         message: 'email or passord does not match',
+      //       });
+      //     }
+      //   }
+      // );
     } catch (error) {
       next(error);
     }
@@ -105,36 +151,93 @@ class AuthController {
       // check if token exists in DB
       // if yes then verify that user and return success message
       // else return error message
-      db.query(
-        `select verified from users where auth_token ='${token}'`,
-        function (err, result) {
-          if (result.length > 0) {
-            if (result[0].verified === 0) {
-              db.query(
-                `UPDATE users SET verified = 1 WHERE auth_token ='${token}'`,
-                function (err, result) {
-                  console.log(result.affectedRows);
 
-                  return response.send({
-                    status: 200,
-                    message: 'Email verified',
-                  });
-                }
-              );
-            } else {
+      
+      await User.findOne({
+        attributes: ['verified'],
+        where: {
+          authToken: token,
+        
+        },
+      }).then(async (result) => {
+
+        if (result) {
+         // console.log(result);
+          if (!result.verified) {
+            User.update({
+              verified:true
+            }, {
+              where: {
+                authToken: token
+              }
+            }).then(()=>{
+
               return response.send({
-                status: 202,
-                message: 'Email already verified',
+                status: 200,
+                authToken:token,
+                message: 'Email verified',
               });
-            }
+
+            });
+            //////////////////////////////
+            // db.query(
+            //   `UPDATE users SET verified = 1 WHERE auth_token ='${token}'`,
+            //   function (err, result) {
+            //     console.log(result.affectedRows);
+
+            //     return response.send({
+            //       status: 200,
+            //       message: 'Email verified',
+            //     });
+            //   }
+            // );
           } else {
             return response.send({
-              status: 400,
-              message: 'Email not verified',
+              status: 202,
+              message: 'Email already verified',
             });
           }
+        } else {
+          return response.send({
+            status: 400,
+            message: 'Email not verified',
+          });
         }
-      );
+
+
+      })
+
+      ////////////////////////////////
+      // db.query(
+      //   `select verified from users where auth_token ='${token}'`,
+      //   function (err, result) {
+      //     if (result.length > 0) {
+      //       if (result[0].verified === 0) {
+      //         db.query(
+      //           `UPDATE users SET verified = 1 WHERE auth_token ='${token}'`,
+      //           function (err, result) {
+      //             console.log(result.affectedRows);
+
+      //             return response.send({
+      //               status: 200,
+      //               message: 'Email verified',
+      //             });
+      //           }
+      //         );
+      //       } else {
+      //         return response.send({
+      //           status: 202,
+      //           message: 'Email already verified',
+      //         });
+      //       }
+      //     } else {
+      //       return response.send({
+      //         status: 400,
+      //         message: 'Email not verified',
+      //       });
+      //     }
+      //   }
+      // );
     } catch (e) {
       next(e);
     }
