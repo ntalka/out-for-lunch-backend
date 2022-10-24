61e136cc7f6b11c6eb32e6dedc179591480b2c90
Zeba Ramzan <zebaramzan@hotmail.com>
2022-10-24 15:28:44 +0300

    remove test and production objects from json-config

--- config/json-config.js
+++ config/json-config.js
@@ -9,19 +9,5 @@ const config = {
     port: db_port,
     dialect: 'mysql',
   },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
 };
 module.exports = config;
