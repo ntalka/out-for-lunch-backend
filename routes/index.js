5fcc4b39ca36979a5dfd7c3dfaea34b5f6d59564
Zeba Ramzan <zebaramzan@hotmail.com>
2022-10-31 16:14:33 +0200

    add cors middleware in login endpoint

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var AuthController = require('../src/controllers/auth/auth.controller');
 var GroupController = require('../src/controllers/group/group.controller');
 var RestaurantController = require('../src/controllers/restaurant/restaurant.controller');
+var cors = require('cors');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -12,7 +13,7 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/signup', AuthController.auth);
-router.post('/login', AuthController.login);
+router.post('/login', cors(), AuthController.login);
 router.post(`/verify/:token`, AuthController.verify);
 
 router.post(
@@ -24,6 +25,6 @@ router.post('/create-cutom-group', GroupController.createCustomGroup); //custom 
 router.post('/create-random-group', GroupController.createRandomGroup);
 router.post('/join-group', GroupController.joinGroup);
 router.post('/join-random-group', GroupController.joinRandomGroup);
-router.get('/get-groups-list', GroupController.getGroupsList);
+router.get('/get-groups-list', cors(), GroupController.getGroupsList);
 
 module.exports = router;
