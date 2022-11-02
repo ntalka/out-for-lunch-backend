93b73633c030cb85d699bf07822e06a63dbdc8ac
Zeba Ramzan <zebaramzan@hotmail.com>
2022-11-02 11:09:39 +0200

    fix cors issue

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var AuthController = require('../src/controllers/auth/auth.controller');
 var GroupController = require('../src/controllers/group/group.controller');
 var RestaurantController = require('../src/controllers/restaurant/restaurant.controller');
-var cors = require('cors');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -13,7 +12,7 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/signup', AuthController.auth);
-router.post('/login', cors(), AuthController.login);
+router.post('/login', AuthController.login);
 router.post(`/verify/:token`, AuthController.verify);
 
 router.post(
@@ -25,6 +24,6 @@ router.post('/create-cutom-group', GroupController.createCustomGroup); //custom 
 router.post('/create-random-group', GroupController.createRandomGroup);
 router.post('/join-group', GroupController.joinGroup);
 router.post('/join-random-group', GroupController.joinRandomGroup);
-router.get('/get-groups-list', cors(), GroupController.getGroupsList);
+router.get('/get-groups-list', GroupController.getGroupsList);
 
 module.exports = router;
