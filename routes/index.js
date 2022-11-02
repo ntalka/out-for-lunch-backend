ec22124fd65e84566035407b0ae5e1b270f937cf
Muhammad Arslan Shahab <muhammadarslan.shahab@tuni.fi>
2022-11-02 12:03:51 +0200

    add time in group details

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require("express");
 var router = express.Router();
-var AuthController = require('../src/controllers/auth/auth.controller');
-var GroupController = require('../src/controllers/group/group.controller');
-var RestaurantController = require('../src/controllers/restaurant/restaurant.controller');
+var AuthController = require("../src/controllers/auth/auth.controller");
+var GroupController = require("../src/controllers/group/group.controller");
+var RestaurantController = require("../src/controllers/restaurant/restaurant.controller");
+var cors = require("cors");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Express',
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Express",
   });
 });
 
-router.post('/signup', AuthController.auth);
-router.post('/login', AuthController.login);
+router.post("/signup", AuthController.auth);
+router.post("/login", cors(), AuthController.login);
 router.post(`/verify/:token`, AuthController.verify);
 
 router.post(
@@ -20,10 +21,10 @@ router.post(
   RestaurantController.getRestaurantListFromAPI
 );
 
-router.post('/create-cutom-group', GroupController.createCustomGroup); //custom group
-router.post('/create-random-group', GroupController.createRandomGroup);
-router.post('/join-group', GroupController.joinGroup);
-router.post('/join-random-group', GroupController.joinRandomGroup);
-router.get('/get-groups-list', GroupController.getGroupsList);
+router.post("/create-cutom-group", GroupController.createCustomGroup); //custom group
+router.post("/create-random-group", GroupController.createRandomGroup);
+router.post("/join-group", GroupController.joinGroup);
+router.post("/join-random-group", GroupController.joinRandomGroup);
+router.get("/get-groups-list", cors(), GroupController.getGroupsList);
 
 module.exports = router;
