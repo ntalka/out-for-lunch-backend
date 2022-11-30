9e0d04f991632922105f5bb4dd4c9fa5e3652355
Zeba Ramzan <zebaramzan@hotmail.com>
2022-11-30 14:23:23 +0200

    test cron job

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const database = require('./config/config');
+const { Office } = require('./models');
 require('dotenv').config();
+const { CronJob } = require('cron');
 const app = require('./app');
 const initApp = () => {
   new Promise((resolve) => {
@@ -7,6 +9,21 @@ const initApp = () => {
     resolve('success');
   }).then(() => {
     const PORT = process.env.PORT || 3000;
+    const restaurantJob = new CronJob(
+      '1-59 * * * *',
+      async () => {
+        await Office.create({
+          name: 'xyz',
+          location: 'abc',
+        });
+        // console.log('job is running');
+      },
+      null,
+      true,
+      'Etc/GMT'
+    );
+
+    restaurantJob.start();
     app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
   });
 };
