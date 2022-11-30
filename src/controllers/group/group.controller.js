aafdab90cb4bad86924ecf0eb0ac10cc2cd6cfe4
Zeba Ramzan <zebaramzan@hotmail.com>
2022-11-30 14:50:11 +0200

    add useful comments for createCustomGroup

--- src/controllers/group/group.controller.js
+++ src/controllers/group/group.controller.js
@@ -1,38 +1,27 @@
 const Models = require('../../../models');
 const moment = require('moment');
 const sequelize = require('sequelize');
-const {
-  Op
-} = sequelize;
-const {
-  Group,
-  GroupMember,
-  User,
-  Restaurant
-} = Models;
+const { Op } = sequelize;
+const { Group, GroupMember, User, Restaurant } = Models;
 
 class GroupController {
-
   /**
-   * Creates custom group
-   * 
-   * @param {number} request Authorization header
-   * @param {number} response 200 for success. 400 for failing
-   * @returns {number}
+   * Creates group with given RestaurantId and Time
+   *
+   * @param {Object} request body must have time in String and restaurantId in String, must have authToken in Authorization header
+   * @param {Object} response 200 for success. 400 for failing
+   * @returns {Object}
    */
 
   async createCustomGroup(request, response, next) {
     const authToken = request.headers.authorization;
     try {
-      let {
-        userId,
-        officeId
-      } = await User.findOne({
-          attributes: ['id', 'officeId'],
-          where: {
-            authToken,
-          },
-        })
+      let { userId, officeId } = await User.findOne({
+        attributes: ['id', 'officeId'],
+        where: {
+          authToken,
+        },
+      })
         .then(async (user) => {
           if (user) {
             return {
@@ -74,11 +63,11 @@ class GroupController {
     const authToken = request.headers.authorization;
     try {
       await User.findOne({
-          attributes: ['id', 'officeId', 'authToken'],
-          where: {
-            authToken,
-          },
-        })
+        attributes: ['id', 'officeId', 'authToken'],
+        where: {
+          authToken,
+        },
+      })
         .then(async (user) => {
           if (user) {
             await Group.findAll({
@@ -89,15 +78,18 @@ class GroupController {
                   [Op.gte]: new Date(), //gte = greater than equal to
                 },
               },
-              include: [{
+              include: [
+                {
                   model: GroupMember,
                   as: 'groupMember',
                   attributes: ['userId'],
-                  include: [{
-                    model: User,
-                    as: 'user',
-                    attributes: ['name'],
-                  }, ],
+                  include: [
+                    {
+                      model: User,
+                      as: 'user',
+                      attributes: ['name'],
+                    },
+                  ],
                 },
                 {
                   model: Restaurant,
@@ -107,7 +99,8 @@ class GroupController {
               ],
               order: [
                 ['time', 'ASC'],
-                [{
+                [
+                  {
                     model: Restaurant,
                     as: 'restaurant',
                   },
@@ -195,15 +188,12 @@ class GroupController {
   async joinRandomGroup(request, response, next) {
     const authToken = request.headers.authorization;
     try {
-      let {
-        userId,
-        officeId
-      } = await User.findOne({
-          attributes: ['id', 'officeId'],
-          where: {
-            authToken,
-          },
-        })
+      let { userId, officeId } = await User.findOne({
+        attributes: ['id', 'officeId'],
+        where: {
+          authToken,
+        },
+      })
         .then(async (user) => {
           if (user) {
             return {
@@ -216,27 +206,25 @@ class GroupController {
           console.log(error);
         });
 
-      const {
-        startTime,
-        endTime
-      } = request.body;
+      const { startTime, endTime } = request.body;
       const groupIds = await Group.findAll({
-          attributes: ['id'],
-          where: {
-            officeId: officeId,
-            [Op.and]: [{
-                time: {
-                  [Op.gte]: new moment(startTime),
-                },
+        attributes: ['id'],
+        where: {
+          officeId: officeId,
+          [Op.and]: [
+            {
+              time: {
+                [Op.gte]: new moment(startTime),
               },
-              {
-                time: {
-                  [Op.lte]: new moment(endTime),
-                },
+            },
+            {
+              time: {
+                [Op.lte]: new moment(endTime),
               },
-            ],
-          },
-        })
+            },
+          ],
+        },
+      })
         .then((result) => {
           if (result) {
             return result.map((res) => res.id);
@@ -273,9 +261,9 @@ class GroupController {
           });
         }
         await GroupMember.create({
-            userId: userId,
-            groupId,
-          })
+          userId: userId,
+          groupId,
+        })
           .then(() => {
             return response.status(200).send({
               message: 'Group joined successfully',
